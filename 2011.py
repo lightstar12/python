f5e025029a04581dcd66aa952fb01f86811cc19f
lightstar12 <37330059+lightstar12@users.noreply.github.com>
2022-02-10 22:12:00 +0900

    Create 2011.py

---
+++ 2011.py
@@ -0,0 +1,9 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        X = 0
+        for i in range(len(operations)):
+            if operations[i] == "++X" or operations[i] == "X++":
+                X = X + 1
+            elif operations[i] == "--X" or operations[i] == "X--":
+                X = X - 1
+        return X

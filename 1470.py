86c328306b0353ac4cae5782e00d7651c3ebf9de
lightstar12 <37330059+lightstar12@users.noreply.github.com>
2022-02-17 20:58:39 +0900

    Create 1470.py

---
+++ 1470.py
@@ -0,0 +1,7 @@
+class Solution:
+  def shuffle(self, nums:List[int], n: int) -> List[int]:
+    numss = []
+    for i in range(n):
+      numss.append(nums[i])
+      numss.append(nums[n+i])
+     return numss

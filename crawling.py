1d415b260ce65665ff70193ca74b51b2639e326c
lightstar12 <37330059+lightstar12@users.noreply.github.com>
2022-02-17 23:29:02 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -1,32 +1,22 @@
 from lib2to3.pgen2 import driver
 from selenium import webdriver
 driver = webdriver.Chrome(executable_path=r"C:\PythonHome\chromedriver.exe")
-driver.get("https://www.pet-friends.co.kr/product/information?product_id=6627")
+driver.get("https://www.pet-friends.co.kr/product/review/list?product_id=6627&package_code=N&is_user_product_review_writable=false")
 
 import time
 wadiz_list = []
 wadiz_body = []
 wadiz_ = {}
 
-table = driver.find_element_by_class_name("review-order-by-container")
-rows = table.find_elements_by_class_name("review-item")
+time.sleep(30)
 
-for i in range(1,5):
-    table = driver.find_element_by_class_name('review-list')
-    rows = table.find_elements_by_class_name("review-item")
-    rows = table.find_elements_by_class_name("review-item")[i]
-    rows.click()
-    time.sleep(3)
+for i in range(1,3):
 
-    body = driver.find_elements_by_xpath(f'//*[@id="app"]/div[2]/div/div[6]/div[16]/div[3]/div[2]/div/div[1]/div[3]')
+    body = driver.find_elements_by_xpath(f'//*[@id="app"]/div[2]/div/div[4]/div/div[4]/div[2]/div/div[1]')
     for value in body:
         wadiz_body.append(value.text)
     wadiz_['body'] = wadiz_body
 
-    time.sleep(2)
-    button = driver.find_element_by_class_name('back-btn')
-    button.click()
-    time.sleep(2)
 print(wadiz_)
 
 import pandas as pd
@@ -34,4 +24,4 @@ import numpy as np
 df1=pd.DataFrame(wadiz_)
 
 import csv
-df1.to_csv("wadiz.csv", mode='w',encoding='utf-8-sig')
+df1.to_csv("petfriend.csv", mode='w',encoding='utf-8-sig')

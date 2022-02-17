794d31edc0d36aa7e0f0eee08990c0af0b4fddaf
lightstar12 <37330059+lightstar12@users.noreply.github.com>
2022-02-17 23:38:26 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -1,18 +1,16 @@
 from lib2to3.pgen2 import driver
 from selenium import webdriver
 driver = webdriver.Chrome(executable_path=r"C:\PythonHome\chromedriver.exe")
-driver.get("https://www.pet-friends.co.kr/product/review/list?product_id=6627&package_code=N&is_user_product_review_writable=false")
+driver.get("https://www.pet-friends.co.kr/main/tab/2")
 
 import time
 wadiz_list = []
 wadiz_body = []
 wadiz_ = {}
-
 time.sleep(30)
 
-for i in range(1,3):
-
-    body = driver.find_elements_by_xpath(f'//*[@id="app"]/div[2]/div/div[4]/div/div[4]/div[2]/div/div[1]')
+for i in range(1,4):
+    body = driver.find_elements_by_xpath(f'//*[@id="app"]/div[2]/div/div[4]/div/div[4]/div[2]/div/div[{i}]/div[3]')
     for value in body:
         wadiz_body.append(value.text)
     wadiz_['body'] = wadiz_body

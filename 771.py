40df1e87f6051a38bcb4205c42d13a86c5df761c
lightstar12 <37330059+lightstar12@users.noreply.github.com>
2022-02-20 20:48:28 +0900

    Update 771.py

--- 771.py
+++ 771.py
@@ -1,8 +1,8 @@
 class Solution:
-  def nuJewelsInStones(self, jewels: str, stones: str) -> int:
-    jewels_ = 0
-    for i in range(len(jewels)):
-      for j in range(len(stones)):
-        if jewels[i] == stones[j]:
-          jewels_ = jewels_ + 1
-    retrun jewels_
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewels_ = 0
+        for i in range(len(jewels)):
+            for j in range(len(stones)):
+                if jewels[i] == stones[j]:
+                    jewels_ = jewels_ + 1
+        return jewels_
